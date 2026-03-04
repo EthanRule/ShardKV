2811e1feb06b8c110684eddeea946e6ffe7f242d
EthanRule <ethan2002@live.com>
2026-03-04 09:38:37 -0800

    scaffholding of hash table functions

--- src/HashTable/hash_table.cpp
+++ src/HashTable/hash_table.cpp
@@ -1,3 +1,16 @@
 // Using the hash slot algorithm we can set this project up for multiple clients and sharding.
 // key range of (or 0 – 16383)
+#include "hash_table.h"
 
+void HashTable::set(std::string key, std::string value) {
+
+}
+
+std::string HashTable::get(std::string key) {
+
+    return "";
+}
+
+void HashTable::deleteItem(std::string key) {
+
+}
--- src/HashTable/hash_table.h
+++ src/HashTable/hash_table.h
@@ -4,9 +4,17 @@
 
 typedef std::array<Node, 16384> table;
 class HashTable {
+private:
     table table;
 
+public:
     void set(std::string key, std::string value);
+
     std::string get(std::string key);
+
     void deleteItem(std::string key);
+
+private:
+    // TODO: Ensure this is 16 bits unsigned.
+    ushort hash(std::string key);
 };
