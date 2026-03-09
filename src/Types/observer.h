73ab6e3eb76d23e67c2db7fd4d543375222bc21e
EthanRule <ethan2002@live.com>
2026-03-09 09:38:18 -0700

    initial observer refactor, 1 publisher, many subscribers

--- src/Types/observer.h
+++ src/Types/observer.h
@@ -4,16 +4,10 @@
 
 class IObserver {
  public:
-  virtual ~IObserver(){};
-  virtual void Update(const std::string &message_from_subject) = 0;
-};
-
-class ISubject {
- public:
-  virtual ~ISubject(){};
-  virtual void Attach(IObserver *observer) = 0;
-  virtual void Detach(IObserver *observer) = 0;
-  virtual void Notify() = 0;
+  virtual void update(const std::string &message_from_subject) = 0;
+  virtual void addSubscriber(IObserver *observer) = 0;
+  virtual void removeSubscriber(IObserver *observer) = 0;
+  virtual void notify() = 0;
 };
 
 /**
@@ -21,81 +15,58 @@ class ISubject {
  * changes.
  */
 
-class Subject : public ISubject {
- public:
-  virtual ~Subject() {
-    std::cout << "Goodbye, I was the Subject.\n";
-  }
-
-  /**
-   * The subscription management methods.
-   */
-  void Attach(IObserver *observer) override {
-    list_observer_.push_back(observer);
-  }
-  void Detach(IObserver *observer) override {
-    list_observer_.remove(observer);
-  }
-  void Notify() override {
-    std::list<IObserver *>::iterator iterator = list_observer_.begin();
-    HowManyObserver();
-    while (iterator != list_observer_.end()) {
-      (*iterator)->Update(message_);
-      ++iterator;
-    }
-  }
-
-  void CreateMessage(std::string message = "Empty") {
-    this->message_ = message;
-    Notify();
-  }
-  void HowManyObserver() {
-    std::cout << "There are " << list_observer_.size() << " observers in the list.\n";
-  }
-
-  /**
-   * Usually, the subscription logic is only a fraction of what a Subject can
-   * really do. Subjects commonly hold some important business logic, that
-   * triggers a notification method whenever something important is about to
-   * happen (or after it).
-   */
-  void SomeBusinessLogic() {
-    this->message_ = "change message message";
-    Notify();
-    std::cout << "I'm about to do some thing important\n";
-  }
-
- private:
-  std::list<IObserver *> list_observer_;
-  std::string message_;
-};
-
 class Observer : public IObserver {
- public:
-  Observer(Subject &subject) : subject_(subject) {
-    this->subject_.Attach(this);
-    std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
-    this->number_ = Observer::static_number_;
-  }
-  virtual ~Observer() {
-    std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
-  }
+public:
+    // // finds another su
+    // Observer(Observer &subject) : subject_(subject) {
+    //     this->subject_.Attach(this);
+    //     ++Observer::static_number_;
+    //     this->number_ = Observer::static_number_;
+    // }
+    IObserver* subscription; // Receives events from this publisher
+    std::list<IObserver*> subscribers; // Sends events to these
+    std::string message_;
+    static int static_number_;
+    int number_;
 
-  void Update(const std::string &message_from_subject) override {
-    message_from_subject_ = message_from_subject;
-    PrintInfo();
-  }
-  void RemoveMeFromTheList() {
-    subject_.Detach(this);
-    std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
-  }
-  void PrintInfo() {
-    std::cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << "\n";
-  }
+    // Should be able to publish event to subscribers, and receive events from publishers its subscribed to.
 
- private:
-  std::string message_from_subject_;
-  Subject &subject_;
-  static int static_number_;
-  int number_;
+    void update(const std::string &message_from_subject) override {
+        message_from_subject_ = message_from_subject;
+        PrintInfo();
+    }
+
+    //
+    void addSubscriber(IObserver *observer) override {
+        subscribers.push_back(observer);
+    }
+
+    void removeSubscriber(IObserver *observer) override {
+        subscribers.remove(observer);
+    }
+
+    void notify() override {
+        std::list<IObserver *>::iterator iterator = subscribers.begin();
+        howManySubscribers();
+        while (iterator != subscribers.end()) {
+            (*iterator)->update(message_);
+            ++iterator;
+        }
+    }
+
+    // This message will need to be generic and handle Command and string types.
+    void CreateMessage(std::string message = "Empty") {
+        this->message_ = message;
+        notify();
+    }
+
+    void howManySubscribers() {
+        std::cout << "There are " << subscribers.size() << " observers in the list.\n";
+    }
+
+    void SomeBusinessLogic() {
+        this->message_ = "change message message";
+        notify();
+        std::cout << "I'm about to do some thing important\n";
+    }
 };
