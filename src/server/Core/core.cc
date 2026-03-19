f4fa20dcf882c09b57d6558666ad45511e0c3e88
EthanRule <ethan2002@live.com>
2026-03-19 07:52:15 -0700

    Core listens to rust_web_server corretly

--- src/server/Core/core.cc
+++ src/server/Core/core.cc
@@ -11,6 +11,36 @@
 #define PORT 8080
 #define SA struct sockaddr
 
+// Function designed for chat between client and server. 
+void func(int connfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    // infinite loop for chat 
+    for (;;) { 
+        bzero(buff, MAX); 
+  
+        // read the message from client and copy it in buffer 
+        read(connfd, buff, sizeof(buff)); 
+        // print buffer which contains the client contents 
+        printf("From client: %s\t To client : ", buff); 
+        bzero(buff, MAX); 
+        n = 0; 
+        // copy server message in the buffer 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+  
+        // and send that buffer to client 
+        write(connfd, buff, sizeof(buff)); 
+  
+        // if msg contains "Exit" then server exit and chat ended. 
+        if (strncmp("exit", buff, 4) == 0) { 
+            printf("Server Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+
 void Core::run() {
     int sockfd, connfd;
     socklen_t len;
@@ -54,7 +84,7 @@ void Core::run() {
         printf("server accept the client...\n");
     }
 
-    // func(connfd);
+    func(connfd);
 
     close(sockfd);
 }
